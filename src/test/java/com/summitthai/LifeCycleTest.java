13cabd9ec0be041d50f8945c1ff0cc845ec68b0c
boyone <boyone.g@gmail.com>
2021-04-30 08:52:53 +0700

    Add JUnit Lifecycle

---
+++ src/test/java/com/summitthai/LifeCycleTest.java
@@ -0,0 +1,36 @@
+package com.summitthai;
+
+import org.junit.jupiter.api.*;
+
+public class LifeCycleTest {
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("After Each");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Test 1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Test 2");
+    }
+}
